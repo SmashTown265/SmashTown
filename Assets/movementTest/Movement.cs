9c81bf0c9fbe5e7022095a8bfe0a4a08b2db3e9f
Joseph Martz <jmartz5@ivytech.edu>
2023-03-16 18:05:22 -0400

    updated movement script, fixed in air speed

--- Assets/movementTest/Movement.cs
+++ Assets/movementTest/Movement.cs
@@ -6,27 +6,33 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    Vector2 movementVector;
+    
     Rigidbody2D rigidbody2d;
     Vector2 jump = Vector2.up;
     Vector2 tempVector = Vector2.zero;
     [Header("Movement Settings")]
     [SerializeField] float moveSpeed;
     [SerializeField] float jumpPower;
-    [SerializeField] float inAirMovementSpeed;
+    [SerializeField] float inAirMovementMultiplier;
+    [SerializeField] float inAirMoveSpeed;
+    [SerializeField] float decelSpeed;
 
     [Header("Bools")]
     public bool isGrounded = true;
     bool isMoving = false;
     public bool doubleJumping = false;
 
+    [Header("Inputs")]
+    public Vector2 movementVector;
 
     // Start is called before the first frame update
     void Start()
     {
         moveSpeed = 5f;
         jumpPower = 1.5f;
-        inAirMovementSpeed = .15f;
+        inAirMovementMultiplier = .15f;
+        inAirMoveSpeed = 4.25f;
+        decelSpeed = 50f;
         // Cache components on start to avoid lag
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
@@ -39,6 +45,8 @@ public class Movement : MonoBehaviour
         {
             // Read the value for the "move" action each event call
             movementVector.x = context.ReadValue<Vector2>().x;
+                if(Mathf.Abs(movementVector.x) > .98f)
+                    movementVector.x = Mathf.Sign(movementVector.x);
             isMoving = true;
             // Change of values located in FixedUpdate() which is called on physics updates (50 per second)
         }
@@ -97,14 +105,25 @@ public class Movement : MonoBehaviour
         // If in the air, and velocity + anticipated movement is less than set movement speed
         // change the current velocity by direction multiplied
         // by the default in air movement speed multiplier
+
+        // TODO: rewrite all this
         else 
         {
-            movementVector.x *= inAirMovementSpeed;
-            if (Mathf.Abs(rigidbody2d.velocity.x + movementVector.x) <= moveSpeed)
+            movementVector.x *= inAirMovementMultiplier;
+            if (Mathf.Abs(rigidbody2d.velocity.x) > inAirMoveSpeed)
+            {
+                tempVector.x = Mathf.Sign(rigidbody2d.velocity.x) * inAirMoveSpeed;
+                tempVector.y = rigidbody2d.velocity.y;
+
+                rigidbody2d.velocity = Vector2.Lerp(rigidbody2d.velocity, tempVector, (moveSpeed - inAirMoveSpeed)/decelSpeed);
+
+                
+            }
+            if (Mathf.Abs(rigidbody2d.velocity.x + movementVector.x) <= inAirMoveSpeed)
             {
                 rigidbody2d.velocity += movementVector;
             }
-            movementVector.x /= inAirMovementSpeed;
+            movementVector.x /= inAirMovementMultiplier;
         }
         
 
