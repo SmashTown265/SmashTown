77de02dabff013a244a666d584a3a8116f7ce861
Joseph Martz <jmartz5@ivytech.edu>
2023-05-07 16:25:58 -0400

    debug push

--- Assets/Scripts/Network/OwnerNetworkAnimator.cs
+++ Assets/Scripts/Network/OwnerNetworkAnimator.cs
@@ -1,7 +1,35 @@
+using Unity.Netcode;
 using Unity.Netcode.Components;
+using UnityEngine;
 
-public class OwnerNetworkAnimator : NetworkAnimator {
-    protected override bool OnIsServerAuthoritative() {
-        return false;
+public class OwnerNetworkAnimator : NetworkBehaviour {
+    private PlayerMovement player1;
+    private PlayerMovement player2;
+    private void Start()
+    {
+        player1 = GameObject.FindWithTag("Player 1").GetComponent<PlayerMovement>();
+        player2 = GameObject.FindWithTag("Player 2").GetComponent<PlayerMovement>();
+    }
+    private void FixedUpdate()
+    {
+        if (IsServer)
+        {
+            PlayerStateClientRpc(player1.playerState);
+        }
+        if (IsClient)
+        {
+            PlayerStateServerRpc(player2.playerState);
+        }
+    }
+    [ClientRpc]
+    void PlayerStateClientRpc(State playerState)
+    {
+        if(!IsServer)
+            player1.playerState = playerState;
+    }
+    [ServerRpc(RequireOwnership = false)]
+    void PlayerStateServerRpc(State playerState)
+    {
+        player2.playerState = playerState;
     }
 }
