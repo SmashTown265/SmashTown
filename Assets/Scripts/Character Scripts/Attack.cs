314d0da9229041b076eb5e6aec6597b8f5af5cd6
Joseph Martz <jmartz5@ivytech.edu>
2023-05-07 17:33:48 -0400

    debug push

--- Assets/Scripts/Character Scripts/Attack.cs
+++ Assets/Scripts/Character Scripts/Attack.cs
@@ -1,36 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+
+using Unity.Netcode;
 using UnityEngine;
 
-public class Attack : MonoBehaviour
+
+public class Attack : NetworkBehaviour
 {
 	public float attackDamage = 5f;
 	private Transform t;
 	public float knockBack = 3f;
     private string playerTag;
-
+    private Health player1;
+    private Health player2;
 	// Start is called before the first frame update
     void Start()
     {
         t = GetComponentsInParent<Transform>(true)[1];
-        playerTag = gameObject.transform.parent.tag;
+        playerTag = gameObject.tag;
+        player1 = GameObject.FindWithTag("Player 1").GetComponent<Health>();
+        player2 = GameObject.FindWithTag("Player 2").GetComponent<Health>();
     }
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
+        if(other.GetType() == typeof(BoxCollider2D))
+            print("Testing colliderType");
 	    if (!other.gameObject.CompareTag(playerTag))
 	    {
             if(knockBack * t.localScale.x < 0)
 				knockBack *= -1;
 		    print($"knockback {knockBack}");
-		    var damage = other.gameObject.GetComponent<Health>();
+            var damage = other.gameObject.GetComponent<Health>();
 		    damage.OnDamage(attackDamage, knockBack);
+            if (IsServer)
+                OnDamageClientRpc(knockBack, attackDamage);
+            else if (!IsServer)
+                OnDamageServerRpc(knockBack, attackDamage);
 	    }
     }
-    // Update is called once per frame
-    void Update()
-    {
+
         
+    [ServerRpc(RequireOwnership = false)]
+    private void OnDamageServerRpc(float knockBack, float attackDamage, ServerRpcParams serverRpcParams = default) 
+    {
+        player1.OnDamage(attackDamage, knockBack);
+    }
+
+    [ClientRpc]
+    private void OnDamageClientRpc(float knockBack, float attackDamage,  ClientRpcParams clientRpcParams = default) 
+    {
+        if (!IsServer)
+        {
+            player2.OnDamage(attackDamage, knockBack);
+        }
     }
 }
--- Assets/Scripts/Character Scripts/Health.cs
+++ Assets/Scripts/Character Scripts/Health.cs
@@ -1,36 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
+
+using Unity.Netcode;
 using UnityEngine;
 
-public class Health : MonoBehaviour
+public class Health : MonoBehaviour 
 {
 	public float damagePercent = 0f;
 
 	private Rigidbody2D rb;
-	private Animator anim;
-
 	private Vector2 knockBackDir = Vector2.zero;
 	// Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
     }
 
     public void OnDamage(float damage, float knockBack)
     {
 	    damagePercent += damage;
-        anim.SetTrigger("Damaged");
-	    print(knockBack);
 	    if (damagePercent < 100)
 	    {
             knockBackDir.Set(knockBack + (damagePercent / 10f) * Mathf.Sign(knockBack), .1f + (damagePercent / 20f));
-            
 	    }
 	    else
 	    {
 		    knockBackDir.Set(knockBack + ((damagePercent / 10f) * Mathf.Sign(knockBack)), .3f + (damagePercent / 10f));
-		    
 
 	    }
 	    
@@ -42,4 +35,5 @@ public class Health : MonoBehaviour
     {
         
     }
+
 }
