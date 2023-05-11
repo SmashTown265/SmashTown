d2a01c8d8a5c0f16b46ec0cdb545721ab3031d87
Joseph Martz <jmartz5@ivytech.edu>
2023-05-11 13:01:41 -0400

    Fixed menu sound options

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,11 +1,13 @@
 using UnityEngine.Audio;
 using System;
 using UnityEngine;
+using System.Linq;
 
 public class AudioManager : MonoBehaviour {
 
-    public Sound[] sounds;
-
+    private Sound[] sounds;
+    public Sound[] music;
+    public Sound[] soundEffects;
     public static AudioManager instance;
 
     // Start is called before the first frame update
@@ -19,14 +21,23 @@ public class AudioManager : MonoBehaviour {
         }
 
         DontDestroyOnLoad(gameObject);
-
-        foreach (Sound s in sounds) {
+        foreach (Sound s in music) {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
+          
         }
+        foreach (Sound s in soundEffects) {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            
+        }
+        
     }
 
     void Start() {
@@ -34,10 +45,28 @@ public class AudioManager : MonoBehaviour {
     }
 
     public void Play (string name) {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
-        if (s == null) {
+        Sound s = Array.Find(music, sound => sound.name == name);
+        if (s == null)
+            s = Array.Find(soundEffects, sound => sound.name == name); 
+        if (s == null)
+        {
+            print("Nope");
             return;
         }
         s.source.Play();
+    }
+    public void OnMusicVolChange(float musicVol)
+    {
+        foreach (Sound s in music)
+        {
+            s.source.volume =  musicVol;
+        }
+    }
+    public void OnSoundEffectVolChange(float sfxVol)
+    {
+        foreach (Sound s in soundEffects)
+        {
+            s.source.volume = sfxVol;
+        }
     }
 }
